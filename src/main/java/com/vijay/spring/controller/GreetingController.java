733fb390de0d14ea41431b3030acb1e79e22f818
vijay <rgopalakrishnan@ebsco.com>
2023-02-28 16:37:18 -0500

    fix(US00004): Change path

--- src/main/java/com/vijay/spring/controller/GreetingController.java
+++ src/main/java/com/vijay/spring/controller/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
 	private static final String template = "Hi, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@GetMapping("/greeting")
+	@GetMapping("/greet")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
