90c0ff49482a9213bbed33a5296b24860f5e7f2a
vijay <rgopalakrishnan@ebsco.com>
2023-02-28 18:27:29 -0500

    chore(US00004): Cleanup code

--- src/main/java/com/vijay/spring/controller/HelloWorldController.java
+++ src/main/java/com/vijay/spring/controller/HelloWorldController.java
@@ -1,11 +1,12 @@
 package com.vijay.spring.controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
     @GetMapping("/hw")
-    public String HelloWorld(){
+    public String HelloWorld() {
         return "Hello World from spring boot";
     }
 }
