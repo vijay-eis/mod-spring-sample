2c38d63c9b5b5598f7c685d74e00f4a929610453
vijay <rgopalakrishnan@ebsco.com>
2023-03-03 07:10:08 -0500

    feat(US00017): Added logging support for the new controller

--- src/main/java/com/vijay/spring/controller/GreetingController.java
+++ src/main/java/com/vijay/spring/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package com.vijay.spring.controller;
 
 import com.vijay.spring.model.Greeting;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@Log4j2
 public class GreetingController {
 
     private static final String template = "Hello, %s!";
@@ -15,6 +17,7 @@ public class GreetingController {
 
     @GetMapping("/greet")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        log.info("Handling index route for feature 1....");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 }
